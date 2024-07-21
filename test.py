3a38f88351c34688b29c043d113c7cdcb81d76e1
Daniel Iglesias Valenzuela <danieliglesias.v@gmail.com>
2024-07-21 13:39:16 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,96 +1,29 @@
-biped_film_pos = {
-  "ches_01": [0,0,0],
-  "ches_02": [0,0,0],
-  "neck_01": [0,0,0]
-}
-
-biped_game_pos = {
-  "root_01": [0,0,0],
-  "ches_01": [0,0,0],
-  "ches_02": [0,0,0]
-}
-
-quadr_game_pos = {
-  "root_01": [0,0,0],
-  "ches_01": [0,0,0],
-  "ches_02": [0,0,0]
-}
-
-quadr_film_pos = {
-  "root_01": [0,0,0],
-  "ches_01": [0,0,0],
-  "ches_02": [0,0,0]
-}
-
-#global variables
-##lets create 2 variables to determinate variables to be able to scale the controllers automaticly base on the distance of the center to top joint
-ctrl_size = 1
-scale_char = 2
-top_jnt = None
-
-def os_system():
-	return os
-
-def global_scale(data = None,ctrl_size = 1):
-	ypos_list = []
-    for item in data:
-    	ypos_list.append(data[item][1])
-   	if max(ypos_list) != 2:
-    	scale_char = max(ypos_list)
+def save_mainstructure_guide(name=None, objectList=None):
+    final_dict = dict()
+    if not objectList:
+        selected = cmds.ls(sl=True)
+        if not selected:
+          	errorMessage('Nothing is selected')
     	
-        ## this result need to be % as a decimal
-    	difference = ((scale_char - 2) * 100) / 2
-    	ctrl_size = ctrl_size * difference
-    else:
-    	ctrl_size = 1
     
-    return ctrl_size    	
-		
+    if len(objectList) >= 2:
 
+        
+        for item in objectList:
+        	emptyDict = dict()
+        	#lets get the parent name
+            parent = cmds.listRelatives(item, parent = True)
+            if not parent:
+            	parent = 'none'
+            #position
+           	xform = cmds.xform(item, t=True, ws=True, q=True)
+             
+            emptyDict.update({'parent': parent})
+            emptyDict.update({'xform': xform})
+            
+            final_dict.update({item: emptyDict})
 
-###auto_riggingUI
-###auto_rigging_controller
+        
 
-
-###auto_rigging_general
-
-def save_guide(name=None, type = None):
-
-def load_guides(name = Name, filename = None):
-
-def build_guides(name = None, dictionary = None):
-
-	guide = cmds.sphere(radius=0.2, name='{}_{}_eyelit_guide'.format(sides, x))
-	
-	return 0
-def build_joint():
-	return 0
-    
-###auto_rigging_biped_chest
-
-###auto_rigging_biped_arms
-###auto_rigging_biped_hands
-###auto_rigging_biped_legs
-
-###auto_rigging_quadr_chest
-###auto_rigging_quadr_legs
-###auto_rigging_quadr_hands
-
-
-###modular_rigging_arms
-###modular_rigging_legs
-
-
-############### EXTRAS ####################
-
-def fk_ik_switch(ctrl):
-
-
-
-#### 06 29 2024 
-
-
-
-	
-
-	
+    directory = 'base/guide/'
+    utili.nameInputWindow(section_dir=directory, dictionary=final_dict)
