08808ae88b1fb776e0115462afffd0a54b43367f
daniel windows <danieliglesias.v@gmail.com>
2025-01-19 01:37:38 -0700

    new tool, adding the main function of selecting part of the body

--- modular_py_tool/sandbox_modular_py_tool.py
+++ modular_py_tool/sandbox_modular_py_tool.py
@@ -16,16 +16,100 @@ auto_rig_ui.modular_ui()
 
 #calculate height character
 
-distance_shape  = cmds.distanceDimension( sp=(0, 0, 0), ep=(0,2,0) )
-transform_node = cmds.listRelatives(distance_shape , parent=True)[0]
-locators = cmds.listConnections(distance_shape , type='locator')
 
-cmds.rename(transform_node,'heightDistance')
-for i,loc in enumerate(locators):
-    if i == 0:
-        cmds.rename(loc,'heightLocA')
-    else:
-        cmds.rename(loc,'heightLocB')
-        
-emptyGrp = utili.createEmptyGroup(name='height')
-[cmds.parent(object1,emptyGrp) for object1 in ('heightDistance','heightLocA','heightLocB')]
+
+f = open('C:/Users/danie/Desktop/Rigging/python_tool/test.json')
+
+
+global global_dict
+#data = json.load(f)
+global_dict = json.load(f)
+side_flag = []
+l_side_flag = False
+r_side_flag = False
+
+
+print(global_dict)
+
+
+#get name of dictionaries
+print(list(data.keys()))
+
+#get name of subdisctionaries
+print(list(data['torso01'].keys()))
+
+#get name of subdisctionaries filter by %guide%
+print([key for key in data['torso01'].keys() if 'guide' in key])
+
+#here we get the specific position 
+print(data['torso01']['guide1']['position'])
+
+
+
+##### now building the disctionary back #####
+
+guide1 = {
+    "position": [1, 0, 0],
+    "bone_ori": "XYZ",
+    "parent": "hip01",
+    "bone_name": "spine01_BIND"
+}
+
+guide2 = {
+    "position": [1, 0, 0],
+    "bone_ori": "XYZ",
+    "parent": "Guide1",
+    "bone_name": "spine02_BIND"
+}
+
+guide3 = {
+    "position": [1, 0, 0],
+    "bone_ori": "XYZ",
+    "parent": "Guide2",
+    "bone_name": "chest01_BIND"
+}
+
+# Create the 'general' sub-dictionary
+general = {
+    "parent": "??"
+}
+
+# Now, build the final nested dictionary for 'torso01'
+torso01 = {
+    "general": general,
+    "guide1": guide1,
+    "guide2": guide2,
+    "guide3": guide3
+}
+
+# Repeat the same process for 'lfarm01'
+lfarm01 = {
+    "general": general,
+    "guide1": guide1,
+    "guide2": guide2,
+    "guide3": guide3
+}
+
+# Combine 'torso01' and 'lfarm01' into a final nested dictionary
+nested_dict = {
+    "torso01": torso01,
+    "lfarm01": lfarm01
+}
+
+
+##### MODIFY GLOBAL DISCTIONARY
+
+def modify_position():
+    global global_dict  # Optional if you're reassigning the whole dictionary
+    global_dict['torso01']['guide1']['position'] = [2, 1, 0]  # Modify existing value
+
+# Function to add a new key-value pair to the global dictionary
+def add_new_guide():
+    global global_dict  # Optional if you're reassigning the whole dictionary
+    global_dict['torso01']['guide2'] = {
+        "position": [0, 1, 0],
+        "bone_ori": "XYZ",
+        "parent": "guide1",
+        "bone_name": "spine02_BIND"
+    }
+
--- test.py
+++ test.py
@@ -1,70 +1,42 @@
-cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[0]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 1),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.0),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.0)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[1]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.999),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.001),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.0)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[2]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.956),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.044),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.0)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[3]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.700),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.300),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.0)])                                
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[4]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.300),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.700),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.0)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[5]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.050),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.950),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.0)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[6]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 1.000),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.000)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[7]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.950),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.050)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[8]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.700),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.300)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[9]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.400),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.600)])                                 
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[10]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.200),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.800)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[11]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.050),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 0.950)]) 
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[12]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 1.000)])
-        cmds.skinPercent('{}_{}_tentacle{}_skincluster'.format(name, side, x),
-                         '{}_{}_tentacle{}_curve.cv[13]'.format(name, side, x),
-                         transformValue=[('{}_{}_tentacle{}_01_sta_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_13_mid_joint'.format(name, side, x), 0.000),
-                                         ('{}_{}_tentacle{}_27_end_joint'.format(name, side, x), 1.000)])  
+
+    'torso01': {
+        'general': {
+        'parent': '??'
+        },'guide1': {
+        'position': [1, 0, 0],
+        'bone_ori': 'XYZ',
+        'parent': 'hip01',
+        'bone_name': 'spine01_BIND'
+        },'guide2': {
+        'position': [1, 0, 0],
+        'bone_ori': 'XYZ',
+        'parent': 'Guide1',
+        'bone_name': 'spine02_BIND'
+        },'guide3': {
+        'position': [1, 0, 0],
+        'bone_ori': 'XYZ',
+        'parent': 'Guide2',
+        'bone_name': 'chest01_BIND'
+        }
+    },'lfarm01': {
+        'general': {
+        'parent': '??'
+        },'guide1': {
+        'position': [1, 0, 0],
+        'bone_ori': 'XYZ',
+        'parent': 'hip01',
+        'bone_name': 'spine01_BIND'
+        },
+        'guide2': {
+        'position': [1, 0, 0],
+        'bone_ori': 'XYZ',
+        'parent': 'Guide1',
+        'bone_name': 'spine02_BIND'
+        },
+        'guide3': {
+        'position': [1, 0, 0],
+        'bone_ori': 'XYZ',
+        'parent': 'Guide2',
+        'bone_name': 'chest01_BIND'
+        }
+    }
